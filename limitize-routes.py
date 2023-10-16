112d04c7c0271c7e888011a7dd71790126c1bba1
Kai Herrero <Kai.herrero@gmail.com>
2023-10-16 12:13:11 -0700

    added deletion by slug in premium list (untested)

--- limitize-routes.py
+++ limitize-routes.py
@@ -7,14 +7,13 @@ import os
 
 DEBUG_MODE = True
 
-premium_route_tag = pd.read_csv("premium_routes.csv",dialect=str,index_col=False)
-
-#premium_route_slug_list = (premium_route_tag.iloc[:, 0]).tolist()
+premium_routes = pd.read_csv('premium_routes.csv', index_col = 'route_slug')
+premium_route_slug_list = (premium_routes.iloc[:, 0]).tolist()
 #premium_route_id_list = (premium_route_tag.iloc[:, 1]).tolist()
 
 # specifies the entry and output folders
-entry_path = "../region/WAS/gtfs"
-ct = str(dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+entry_path = '../region/WAS/gtfs'
+ct = str(dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
 output_path = os.path.join(os.getcwd(), "run-" + ct)
 os.mkdir(output_path)
 
@@ -25,7 +24,7 @@ index1 = 0
 for curr_dated_entry in dated_entries:
 
     # Make target output folder, this will look like: "../ted-data/runs/20XX-XX-XX-LIMITED"
-    dated_output_path = os.path.join(output_path, curr_dated_entry + "-limited")
+    dated_output_path = os.path.join(output_path, curr_dated_entry + '-limited')
     os.mkdir(dated_output_path)
 
     zip_entries = os.listdir(os.path.join(entry_path, curr_dated_entry))
@@ -36,15 +35,32 @@ for curr_dated_entry in dated_entries:
 
         # Find entry zip folder
         curr_zip_dir = os.path.join(entry_path, curr_dated_entry, curr_zip_entry)
-
+        curr_zip_slug = curr_zip_entry.removesuffix('.zip')
         if DEBUG_MODE:
-            print("Currently parsing: " + curr_dated_entry + ": " + curr_zip_entry)
+                print("Currently parsing: " + curr_dated_entry + ": " + curr_zip_entry)
 
-        try:
-            remove_routes_from_gtfs(curr_zip_dir, dated_output_path, premium_route_id_list)
-        except zipfile.BadZipFile:
-            if DEBUG_MODE:
-                print(curr_zip_entry, "is not a zipfile, skipping...")
+        #check if the current feed's slug is in premium routes list
+        if (curr_zip_slug in premium_routes['route_slug']):
+
+            curr_slug_rows = premium_routes.loc[premium_routes['route_slug'] == curr_zip_slug]
+            curr_slug_premium_ids = (curr_slug_rows.iloc[:,1]).tolist()
+
+            #delete slug labelled __ALL__
+            if (curr_slug_premium_ids[0] == '__ALL__'):
+                os.remove(curr_zip_dir)
+
+            #delete specific routes within the given slug
+            else:
+                try:
+                    gtfs = GTFS.load_zip(curr_zip_dir)
+                    gtfs.delete_routes(curr_slug_premium_ids)
+
+                    if not os.path.exists(dated_output_path):
+                        os.mkdir(dated_output_path)
+                    gtfs.write_zip(os.path.join(dated_output_path, curr_zip_dir))
+
+                except zipfile.BadZipFile:
+                    if DEBUG_MODE: print(curr_zip_entry, "is not a zipfile, skipping...")
 
     if DEBUG_MODE:
         print("\nFinished parsing: " + curr_dated_entry + "\n")
