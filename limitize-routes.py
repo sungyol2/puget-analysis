f376b4417a4da97987cf0eed927bb3343705aaec
Kai Herrero <Kai.herrero@gmail.com>
2023-10-16 13:29:50 -0700

    syntax correct, gives weird output

--- limitize-routes.py
+++ limitize-routes.py
@@ -7,8 +7,8 @@ import os
 
 DEBUG_MODE = True
 
-premium_routes = pd.read_csv('premium_routes.csv', index_col = 'route_slug')
-premium_route_slug_list = (premium_routes.iloc[:, 0]).tolist()
+premium_routes = pd.read_csv('premium_routes.csv', index_col = False)
+#premium_route_slug_list = (premium_routes.iloc[:, 0]).tolist()
 #premium_route_id_list = (premium_route_tag.iloc[:, 1]).tolist()
 
 # specifies the entry and output folders
@@ -36,11 +36,12 @@ for curr_dated_entry in dated_entries:
         # Find entry zip folder
         curr_zip_dir = os.path.join(entry_path, curr_dated_entry, curr_zip_entry)
         curr_zip_slug = curr_zip_entry.removesuffix('.zip')
-        if DEBUG_MODE:
+        
+        if DEBUG_MODE and not(curr_zip_entry.startswith('._')):
                 print("Currently parsing: " + curr_dated_entry + ": " + curr_zip_entry)
 
         #check if the current feed's slug is in premium routes list
-        if (curr_zip_slug in premium_routes['route_slug']):
+        if (curr_zip_slug in premium_routes['route_slug'].values):
 
             curr_slug_rows = premium_routes.loc[premium_routes['route_slug'] == curr_zip_slug]
             curr_slug_premium_ids = (curr_slug_rows.iloc[:,1]).tolist()
@@ -51,13 +52,13 @@ for curr_dated_entry in dated_entries:
 
             #delete specific routes within the given slug
             else:
-                try:
+                try: #this is the same as gtfs_delete_routes(), rewritten incase that function changes
                     gtfs = GTFS.load_zip(curr_zip_dir)
                     gtfs.delete_routes(curr_slug_premium_ids)
-
+                    zipfile_name = os.path.basename(curr_zip_dir)
                     if not os.path.exists(dated_output_path):
                         os.mkdir(dated_output_path)
-                    gtfs.write_zip(os.path.join(dated_output_path, curr_zip_dir))
+                    gtfs.write_zip(os.path.join(dated_output_path, zipfile_name))
 
                 except zipfile.BadZipFile:
                     if DEBUG_MODE: print(curr_zip_entry, "is not a zipfile, skipping...")
